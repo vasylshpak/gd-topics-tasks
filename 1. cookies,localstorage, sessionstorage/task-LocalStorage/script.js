145c5898516a117118f796faaffaef1c5b36d4ed
nmozhara-gd <77800016+nmozhara-gd@users.noreply.github.com>
2021-06-08 11:53:55 +0200

    Update script.js

--- 1. cookies,localstorage, sessionstorage/task-LocalStorage/script.js
+++ 1. cookies,localstorage, sessionstorage/task-LocalStorage/script.js
@@ -1,6 +1,9 @@
+// ';' in the end of each line
+
 const area = document.getElementById('area')
 const clean = document.getElementById('clear')
 
+// name of localStorage prop should be saved as separate const, in case you want rename in, all you need to do is rename it in one place
 area.value = localStorage.getItem('textArea')
 
 //oninput ??
