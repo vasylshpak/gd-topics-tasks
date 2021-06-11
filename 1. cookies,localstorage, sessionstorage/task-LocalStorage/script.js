1c1e35c9b7b705d41dc337bfc15a6be8dd095b21
Vasyl Shpak <vasyl.shpak4@gmail.com>
2021-06-11 17:13:51 +0200

    refactor

--- 1. cookies,localstorage, sessionstorage/task-LocalStorage/script.js
+++ 1. cookies,localstorage, sessionstorage/task-LocalStorage/script.js
@@ -1,14 +1,15 @@
-const area = document.getElementById('area');
-const clean = document.getElementById('clear');
+const area = document.querySelector(".area");
+const clean = document.querySelector(".clear");
+const textArea = "textArea";
 const inputStorage = window.localStorage;
 
-area.value = inputStorage.getItem('textArea');
+area.value = inputStorage.getItem(textArea);
 
-area.addEventListener('input', event => {
-    inputStorage.setItem('textArea', area.value);
+area.addEventListener("input", (event) => {
+  inputStorage.setItem(textArea, area.value);
 });
 
-clean.addEventListener('click', event => {
-    inputStorage.removeItem('textArea');
-    area.value = '';
+clean.addEventListener("click", (event) => {
+  inputStorage.removeItem(textArea);
+  area.value = "";
 });
