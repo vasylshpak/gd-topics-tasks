ad7e91bf623563df037e79b935c22849a5950eb9
Vasyl Shpak <vasyl.shpak4@gmail.com>
2021-06-14 15:31:11 +0200

    add: promise circle task solution

---
+++ 3.Promises-draw-circle/draw-circle-task-3/script.js
@@ -0,0 +1,20 @@
+function showCircle(cx, cy, radius) {
+  let div = document.createElement("div");
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + "px";
+  div.style.top = cy + "px";
+  div.className = "circle";
+  document.body.append(div);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + "px";
+      div.style.height = radius * 2 + "px";
+      div.addEventListener("transitionend", () => resolve(div));
+    });
+  }).then((div) => {
+    div.classList.add("message");
+    div.append("Hello, world!");
+  });
+}
